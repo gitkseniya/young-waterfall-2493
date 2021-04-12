87faf8904fd6a252bfa26cae2d63e0967e9f0cd8
Kseniya K <kseniya.kudelich@gmail.com>
2021-04-12 12:00:16 -0600

    Add incomplete user story 2

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,4 +3,8 @@ class Movie < ApplicationRecord
 
   has_many :movie_actors
   has_many :actors, through: :movie_actors
+
+  def self.correct_actors
+    joins(movies: { movie_actors: :actor }).where(currently_working: true).order(age: :DESC).distinct
+  end
 end
--- spec/features/studios/show_spec.rb
+++ spec/features/studios/show_spec.rb
@@ -14,4 +14,31 @@ RSpec.describe 'studios show view' do
     expect(page).to have_content(movie1.title)
     expect(page).to have_content(movie2.title)
   end
+
+  it 'shows a list of actors that have acted in any of the studios movies' do
+    studio1 = create(:studio)
+
+    movie1 = create(:movie, studio: studio1)
+    movie2 = create(:movie, studio: studio1)
+
+    actor1 = create(:actor, age: 30)
+    actor2 = create(:actor, age: 55)
+    actor3 = create(:actor, age: 41)
+
+    MovieActor.create(movie: movie1, actor: actor1)
+    MovieActor.create(movie: movie1, actor: actor2)
+    MovieActor.create(movie: movie1, actor: actor3)
+
+    MovieActor.create(movie: movie2, actor: actor1)
+    MovieActor.create(movie: movie2, actor: actor2)
+
+    visit "/studios/#{studio1.id}"
+
+    expect(page).to have_content(actor1.name)
+    expect(page).to have_content(actor2.name)
+    expect(page).to have_content(actor3.name)
+
+    expect(studio1.unique_actors.count).to eq(3)
+    expect(actor2).to appear_before(actor1)
+  end
 end
